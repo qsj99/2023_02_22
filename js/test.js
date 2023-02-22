2a774abc8f283ca1d692224284ea90441d6560c4
sojeong <sojeong8831@daum.net>
2023-02-22 18:06:40 +0900

    02-22

---
+++ js/test.js
@@ -0,0 +1,18 @@
+var user_name = "블랙자몽" ; 
+var user_num = 379 ;
+document.write('javascript이름=', user_name, '<br>') ;
+document.write('javascript숫자=', user_num) ;
+document.write('<hr size=5 color=blue>' );
+
+//국어=90 영어=85 총점=175 평균=87.5
+
+var KOR = 90;
+var ENG = 85;
+var total= KOR+ENG;
+var avg=total/2;
+document.write('국어점수= ',KOR, '<br>');
+document.write('영어점수= ',ENG , '<br>');
+document.write('총점= ', total , '<br>');
+document.write('평균= ',avg , '<br>');
+
+
